01af7c3286c5417e6622317edc0d6fb81af1c947
zaabu <muhumuzabr@gmail.com>
2019-10-29 12:46:44 +0300

    Initial Commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const config = {
+  apikey: 'c8560969e99849978a6f8e98703efeb3'
+};
+export default config;
---
+++ src/client/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="page-footer red">
+      <div className="footer-copyright">
+        <div className="container">
+          © 2019 Fast News - Muhumuza Brian
+        </div>
+      </div>
+    </footer>
+  );
+};
